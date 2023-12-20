a1018a65c9fdf78c1727c6e17f48b93e59703bfd
Natchnene <kondratsiuk.marina@gmail.com>
2023-12-20 19:07:03 +0300

    add password validation

--- mobi_market/users/serializers.py
+++ mobi_market/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.exceptions import AuthenticationFailed
 from django.contrib import auth
+from django.core.exceptions import ValidationError
 from .models import User
 
 
@@ -17,10 +18,19 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'password_check']
 
     def validate(self, data):
-        if data['password'] != data['password_check']:
+        password = data['password']
+        password_check = data['password_check']
+
+        if password != password_check:
             raise serializers.ValidationError(
                 self.default_error_messages['password_mismatch']
             )
+
+        try:
+            auth.password_validation.validate_password(password)
+        except ValidationError as e:
+            raise serializers.ValidationError(str(e))
+
         return data
 
     def create(self, validated_data):
