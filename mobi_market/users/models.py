1f0af5702a94d4372c37af5202afd5591e27fbfd
Natchnene <kondratsiuk.marina@gmail.com>
2023-12-26 17:00:29 +0300

    create new model PhoneNumber

--- mobi_market/users/models.py
+++ mobi_market/users/models.py
@@ -21,11 +21,18 @@ class PersonalData(models.Model):
     name = models.CharField(max_length=200, blank=False)
     last_name = models.CharField(max_length=200, blank=False)
     photo = models.ImageField(null=True, blank=True)
-    phone_number = models.CharField(max_length=15, blank=False, unique=True)
-    code_activation = models.PositiveIntegerField(blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.phone_number} {self.last_name} {self.name}'
+        return f'{self.last_name} {self.name}'
+
+
+class PhoneNumber(models.Model):
+    phone_number = models.CharField(max_length=15, blank=False, unique=True)
+    code_activation = models.PositiveIntegerField(blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.phone_number}'
 
